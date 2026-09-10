37c46bf7680f692bc2e933b7e1a1b560ca807739
Vlad Kamenkov <70900621+VladlenKam@users.noreply.github.com>
2026-09-10 16:35:54 +0300

    Add files via upload

---
+++ oop_9/Task1.py
@@ -0,0 +1,54 @@
+class Trainee:
+    def __init__(self, name: str, surname: str, score: int = 0, passing_grade: int = 10):
+        self.name: str = name
+        self.surname: str = surname
+        self.passing_grade: int = passing_grade
+        self.__score: int = score
+
+    @property
+    def score(self) -> int:
+        return self.__score
+
+    @score.setter
+    def score(self, value: int):
+        if not isinstance(value, int):
+            raise ValueError(f"Expected value of type int, got {type(value)}")
+        if value < 0:
+            raise ValueError("The score shouldn't be less than 0!")
+        self.__score = value
+
+    def do_homework(self) -> None:
+        """Increases score by 1"""
+        self.score += 1
+
+    def miss_homework(self) -> None:
+        """Decreases score by 1"""
+        self.score -= 1
+
+    def visit_lecture(self) -> None:
+        """Increases score by 1"""
+        self.score += 1
+
+    def miss_lecture(self) -> None:
+        """Decreases score by 1"""
+        self.score -= 1
+
+    def is_passing(self) -> bool:
+        return self.score >= self.passing_grade
+
+
+# Тестовые данные
+trainee = Trainee(name="Иван", surname="Иванов", score=9, passing_grade=10)
+
+print("== ПРОВЕРКА УСПЕВАЕМОСТИ СТАЖЕРА ==")
+
+trainee.do_homework()
+print(f"Баллы: {trainee.score}, Прошел курс: {trainee.is_passing()}")
+
+trainee.miss_lecture()
+print(f"Баллы: {trainee.score}, Прошел курс: {trainee.is_passing()}")
+
+try:
+    trainee.score = -5
+except ValueError as e:
+    print(f"Ошибка: {e}")
